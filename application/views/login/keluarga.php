f2df887c7a77ae7a71cea4164fe1f226c6e5b863
Yordan Trivada Bian <yordanbian@gmail.com>
2025-06-21 17:10:47 +0700

    membuat qr code scanner untuk login pegguna

--- application/views/login/keluarga.php
+++ application/views/login/keluarga.php
@@ -1,71 +1,76 @@
-<?php 
+<?php
 $this->load->view('frontend/layouts/header');
 ?>
-	<!-- Main content -->
+<!-- Main content -->
 <div class="main-content">
-    <!-- Header -->
-    <div class="header bg-gradient-primary py-7 py-lg-8 pt-lg-9">
-      <div class="container">
-        <div class="header-body text-center mb-7">
-          <div class="row justify-content-center">
-            <div class="col-xl-5 col-lg-6 col-md-8 px-5">
-              <h1 class="text-white">Selamat Datang</h1>
-              <p class="text-lead text-white">
-	              Apps Keluarga Jemaat
-	              <br>
-	              GKP Kampung Sawah
-	              <br>
-	              <small>Ver. 0.99 Beta</small>
-	          </p>
-            </div>
+  <!-- Header -->
+  <div class="header bg-gradient-primary py-7 py-lg-8 pt-lg-9">
+    <div class="container">
+      <div class="header-body text-center mb-7">
+        <div class="row justify-content-center">
+          <div class="col-xl-5 col-lg-6 col-md-8 px-5">
+            <h1 class="text-white">Selamat Datang</h1>
+            <p class="text-lead text-white">
+              Apps Keluarga Jemaat
+              <br>
+              GKP Kampung Sawah
+              <br>
+              <small>Ver. 0.99 Beta</small>
+            </p>
+            <!-- <div id="my-qr-reader"></div> -->
           </div>
         </div>
       </div>
-      <div class="separator separator-bottom separator-skew zindex-100">
-        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
-          <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
-        </svg>
-      </div>
     </div>
-    <!-- Page content -->
-    <div class="container mt--9 pb-5">
-      <div class="row justify-content-center">
-        <div class="col-lg-5 col-md-7">
-          <div class="card bg-secondary border-0 mb-0">
-            <div class="card-body px-lg-5 py-lg-5">
-              <div class="text-center text-muted mb-4">
-                <small>Masuk dengan ID Pengguna</small>
+    <div class="separator separator-bottom separator-skew zindex-100">
+      <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+      </svg>
+    </div>
+  </div>
+  <!-- Page content -->
+  <div class="container mt--9 pb-5">
+    <div class="row justify-content-center">
+      <div class="col-lg-5 col-md-7">
+        <div class="card bg-secondary border-0 mb-0">
+          <div class="card-body px-lg-5 py-lg-5">
+
+            <!-- qr code scanner -->
+            <div id="my-qr-reader"></div>
+
+            <!-- user id login form -->
+            <form class="d-none" role="form" method="POST" action="<?= base_url(); ?>login/auth_user_keluarga">
+              <div class="form-group mb-3">
+                <div class="input-group input-group-merge input-group-alternative">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="ni ni-single-02"></i></span>
+                  </div>
+                  <input class="form-control" placeholder="ID Pengguna" type="text" name="username">
+                </div>
               </div>
-              <form role="form" method="POST" action="<?=base_url();?>login/auth_user_keluarga">
-                <div class="form-group mb-3">
-                  <div class="input-group input-group-merge input-group-alternative">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="ni ni-single-02"></i></span>
-                    </div>
-                    <input class="form-control" placeholder="ID Pengguna" type="text" name="username">
+              <div class="form-group">
+                <div class="input-group input-group-merge input-group-alternative">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
                   </div>
+                  <input class="form-control" placeholder="Kata Sandi" type="password" name="password">
                 </div>
-                <div class="form-group">
-                  <div class="input-group input-group-merge input-group-alternative">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
-                    </div>
-                    <input class="form-control" placeholder="Kata Sandi" type="password" name="password">
-                  </div>
-                </div>
-                <!--<div class="custom-control custom-control-alternative custom-checkbox">
-                  <input class="custom-control-input" id=" customCheckLogin" type="checkbox">
-                  <label class="custom-control-label" for=" customCheckLogin">
-                    <span class="text-muted">Remember me</span>
-                  </label>
-                </div>-->
-                <div class="text-center">
-                  <button type="submit" class="btn btn-primary my-4">Masuk</button>
-                </div>
-              </form>
+              </div>
+              <div class="text-center">
+                <button type="submit" class="btn btn-primary my-4">Masuk</button>
+              </div>
+            </form>
+
+            <div class="text-center">
+              <div class="my-4">
+                <bold">atau masuk dengan...</bold>
+              </div>
+              <!-- <button class="btn btn-primary my-4">Scan QR</button> -->
+              <button class="btn btn-danger" id="change-login-btn">User ID</button>
             </div>
           </div>
-          <!--<div class="row mt-3">
+        </div>
+        <!--<div class="row mt-3">
             <div class="col-6">
               <a href="#" class="text-light"><small>Forgot password?</small></a>
             </div>
@@ -73,32 +78,52 @@ $this->load->view('frontend/layouts/header');
               <a href="#" class="text-light"><small>Create new account</small></a>
             </div>
           </div>-->
-        </div>
       </div>
     </div>
+  </div>
 </div>
 
-<?php 
+<script src="https://unpkg.com/html5-qrcode"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>/assets/frontend/js/qrCodeScanner.js"></script>
+<script>
+  const myQRReader = document.getElementById('my-qr-reader');
+  const form = document.querySelector('form');
+  const changeLoginBtn = document.getElementById('change-login-btn');
+
+  changeLoginBtn.addEventListener('click', function() {
+    // Toggle visibility of QR reader and form
+    myQRReader.classList.toggle('d-none');
+    form.classList.toggle('d-none');
+
+    if (changeLoginBtn.innerText === 'User ID') {
+      changeLoginBtn.innerText = 'Scan QR';
+    } else {
+      changeLoginBtn.innerText = 'User ID';
+    }
+  });
+</script>
+
+<?php
 $this->load->view('frontend/layouts/footer');
 ?>
 
-<?php 
-if($this->session->userdata('sess_token_valid') ==-1 ){
-	//ini bearti login gagal
-?>
-	<script type="text/javascript">
-		iziToast.error({
-          title: '',
-          message: 'Maaf, Proses Login Gagal! (ID Penggunan dan Kata Sandi tidak cocok)',
-          position: "topRight",
-        });
-	</script>
 <?php
-	$this->session->unset_userdata('sess_token');
-    $this->session->unset_userdata('sess_token_valid');
-    $this->session->unset_userdata('sess_keluarga');
-    $this->session->unset_userdata('sess_nama_user');
-    $this->session->unset_userdata('sess_id_user');
-    $this->session->unset_userdata('sess_num_login');
+if ($this->session->userdata('sess_token_valid') == -1) {
+  //ini bearti login gagal
+?>
+  <script type="text/javascript">
+    iziToast.error({
+      title: '',
+      message: 'Maaf, Proses Login Gagal! (ID Penggunan dan Kata Sandi tidak cocok)',
+      position: "topRight",
+    });
+  </script>
+<?php
+  $this->session->unset_userdata('sess_token');
+  $this->session->unset_userdata('sess_token_valid');
+  $this->session->unset_userdata('sess_keluarga');
+  $this->session->unset_userdata('sess_nama_user');
+  $this->session->unset_userdata('sess_id_user');
+  $this->session->unset_userdata('sess_num_login');
 }
 ?>
